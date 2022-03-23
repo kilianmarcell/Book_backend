8e2b83494f4351d6b371039a8967f418f9421d1b
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-23 11:58:54 +0100

    Listázás javítva

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $visible = [
+        'id',
+        'title',
+        'author',
+        'publish_year',
+        'page_count'
+    ];
 }
