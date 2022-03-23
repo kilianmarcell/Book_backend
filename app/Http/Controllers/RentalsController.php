ba674fb3864bfa7992ff7b655a8a8977b4607300
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-03-23 11:29:42 +0100

    Rentals/Book hozzáadva

---
+++ app/Http/Controllers/RentalsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rentals;
+use Illuminate\Http\Request;
+
+class RentalsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Rentals  $rentals
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Rentals  $rentals
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Rentals  $rentals
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Rentals  $rentals
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rentals $rentals)
+    {
+        //
+    }
+}
